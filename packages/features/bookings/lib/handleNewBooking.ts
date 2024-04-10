6093e8a6b21ffec09fa268441f56a64392a73542
Udit Takkar <udit222001@gmail.com>
2024-04-10 14:00:42 +0530

    fix: type error

--- packages/features/bookings/lib/handleNewBooking.ts
+++ packages/features/bookings/lib/handleNewBooking.ts
@@ -534,6 +534,7 @@ export async function getBookingData<T extends z.ZodType>({
       calEventUserFieldsResponses: undefined,
       calEventResponses: undefined,
       customInputs: undefined,
+      attendeePhoneNumber: undefined,
     };
   }
   if (!reqBody.responses) {
@@ -640,6 +641,7 @@ async function createBooking({
         name: member.name,
         timeZone: member.timeZone,
         locale: member.language.locale,
+        phoneNumber: member.phoneNumber,
       }))
     );
   }
